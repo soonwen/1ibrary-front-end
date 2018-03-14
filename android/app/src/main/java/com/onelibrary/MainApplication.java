b365a074ea8fc81c4ff799ccb9c244581eda1f32
soonwen <13434105@qq.com>
2018-03-14 09:49:34 +0800

    MainApplication.java:6: 错误: 程序包com.cboy.rn.splashscreen不存在 import com.cboy.rn.splashscreen.SplashScreen
    把com.cboy.rn 改为org.devio.rn编译过了

--- android/app/src/main/java/com/onelibrary/MainApplication.java
+++ android/app/src/main/java/com/onelibrary/MainApplication.java
@@ -3,7 +3,7 @@ package com.onelibrary;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
